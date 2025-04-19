ae20025a9c8c21edf6a40cf6c96560c996a7ce00
Michael Kreil <github@michael-kreil.de>
2025-04-19 19:38:27 +0200

    add popup

--- config.ts
+++ config.ts
@@ -1,7 +1,8 @@
 
-//export const date = '2025-04-05';
-//export const ignoreIds = [729, 782, 854, 886, 888, 898, 920, 1106, 1131, 1178, 1215, 1218, 1307];
+export const date = '2025-04-05';
+export const ignoreIds = [729, 782, 854, 886, 888, 898, 920, 1106, 1131, 1178, 1215, 1218, 1307];
 
+/*
 export const date = '2025-04-19';
 export const ignoreIds = [
 	1329,
@@ -48,3 +49,4 @@ export const ignoreIds = [
 	2332,
 	2341
 ];
+*/
