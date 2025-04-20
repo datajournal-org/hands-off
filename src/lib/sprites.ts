71438da11e42bba6c91699b35dc0e14e54ba3420
Michael Kreil <github@michael-kreil.de>
2025-04-21 01:53:11 +0200

    add tennessee

--- src/lib/sprites.ts
+++ src/lib/sprites.ts
@@ -32,7 +32,7 @@ export async function getImages(userSources: UserSource[]): Promise<{ sprites: s
 		}
 	}
 
-	if (sprites.length > 7) sprites.length = 7;
+	if (sprites.length > 6) sprites.length = 6;
 
 	return { sprites, sources };
 }
